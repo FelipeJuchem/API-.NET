f19b8e4e2a29826542587fc0f84b274f829f1546
Felipe Juchem <felipe_build@hotmail.com>
2021-02-22 18:01:51 -0300

    Pro

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace ProAgil.API
             services.AddDbContext<DataContext>(
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,7 @@ namespace ProAgil.API
                 app.UseHsts();
             }
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseHttpsRedirection();
             app.UseMvc();
         }
